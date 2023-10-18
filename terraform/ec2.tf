c47320c721e3c6012b266b7662d52bc056014fcf
Lucas Livrone <lucaslivrone@gmail.com>
2023-10-18 17:26:14 -0300

    Add Terraform files

---
+++ terraform/ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "monitored_server" {
+  count                = 2
+  ami                  = data.aws_ami.ami.id
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  security_groups      = [aws_security_group.sg.name]
+
+  user_data = file("../scripts/user_data.sh")
+
+  tags = {
+    Name = "monitored-server-${count.index + 1}"
+  }
+}
