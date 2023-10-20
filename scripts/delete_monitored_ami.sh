12923f849b7e98107a3aa393a9283940c7ee4fb9
Lucas Livrone <lucaslivrone@gmail.com>
2023-10-20 13:47:18 -0300

    Updated workflow with re-existing AMI check

--- scripts/delete_monitored_ami.sh
+++ scripts/delete_monitored_ami.sh
@@ -5,5 +5,13 @@ ami_name="monitored-ami"
 # Get the AMI ID by filtering on the name
 ami_id=$(aws ec2 describe-images --filters "Name=name,Values=${ami_name}" --query 'Images[0].ImageId' --output text)
 
-# Deregister the AMI using the retrieved AMI ID
-aws ec2 deregister-image --image-id $ami_id
+if [ -z "$ami_id" ]; then
+  echo "AMI not found"
+else
+  # Deregister the AMI using the retrieved AMI ID
+  if aws ec2 deregister-image --image-id $ami_id; then
+    echo "AMI deleted"
+  else
+    echo "Failed to delete AMI"
+  fi
+fi
