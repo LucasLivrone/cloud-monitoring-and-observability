e0c053d286ee885fa59dccba4865c597e0016350
Lucas Livrone <lucaslivrone@gmail.com>
2023-10-20 13:56:43 -0300

    Updated AMI deletion script

--- scripts/delete_monitored_ami.sh
+++ scripts/delete_monitored_ami.sh
@@ -5,13 +5,13 @@ ami_name="monitored-ami"
 # Get the AMI ID by filtering on the name
 ami_id=$(aws ec2 describe-images --filters "Name=name,Values=${ami_name}" --query 'Images[0].ImageId' --output text)
 
-if [ -z "$ami_id" ]; then
-  echo "AMI not found"
-else
+if [[ $ami_id == ami-* ]]; then
   # Deregister the AMI using the retrieved AMI ID
   if aws ec2 deregister-image --image-id $ami_id; then
     echo "AMI deleted"
   else
-    echo "Failed to delete AMI"
+    echo "Failed to delete AMI with ID: $ami_id"
   fi
+else
+  echo "No valid AMI ID found"
 fi
